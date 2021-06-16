8c0ea5d080691e900e07fae676f9aee4e3524c8e
hyuk7924 <hyuk7924@gmail.com>
2021-06-16 15:20:00 +0900

    6

--- get_char:put_char.c
+++ get_char:put_char.c
@@ -11,14 +11,21 @@ int main()
     // 한 글자씩 처리하고 있음 이 사실이 매우 중요함!!!(feat. 버퍼)
     while ( (ch = getchar()) != '\n')
     {
+        if (ch >= 'a' && ch <= 'z') // 'A' = 65, 'a' = 97
+            ch -= 'a' - 'A';
+        else if (ch >= 'A' && ch <= 'Z')
+            ch += 'a' - 'A';
+
+
         /*for (int i = 0; i <= '9'; ++i)
             if (ch == i)
                 ch = '*'; */
-        if (ch >= '0' && ch <= '9')
+        /* if (ch >= '0' && ch <= '9')
              ch = '*';
 
         if(ch =='f' || ch =='F')
-            ch = 'X';
+            ch = 'X'; */
+
         //else if(ch =='F')
         //    ch = 'X';
 
